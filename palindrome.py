7a6932058b3477ac2358515f9470e804f8906c0c
BlakHIT <72243931+BlakHIT@users.noreply.github.com>
2020-10-02 22:42:24 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,22 @@
-num = int(input('Enter a number: '))
-print()
-n = num
-while True:
-    a = str(num)
-    if a[::-1] == a:
-        print('The next palindrome of {} is {}'.format(n, a))
-        break
-    else:
-        n += 1
+total = int(input('Enter the no of numbers you want to find the palindrome of: '))
+lst = []
+for i in range(total):
+    num = int(input('Enter a number: '))
+    print()
+    lst.append(num)
+    
+for values in lst:
+    a = value
+    while True:
+        n = str(a)
+        
+        if str (n[::-1]) == str (n):
+            if a == value:
+                print('{} itself is a palindrome'.format(value))
+                break
+            else:
+                print('The next palindrome of {} is {}'.format(n, a))
+                break
+        a += 1
         
   
