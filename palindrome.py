4f9a93418dc2a917c0d12701113787b9eda80b94
aarushmagotra <70463487+aarushmagotra@users.noreply.github.com>
2022-10-01 11:42:13 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,12 @@
 num = int(input('Enter a number: '))
-print()
+print("-------------------------")
 n = num
+
 while True:
     a = str(num)
     if a[::-1] == a:
         print('The next palindrome of {} is {}'.format(n, a))
         break
     else:
-        n += 1
-        
+        n += 1 
 #code ends here  
